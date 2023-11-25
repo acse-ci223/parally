9e0935a78fef5d077e17078af1f6fcf0c5f58b4d
Chris Ioannidis <142539198+acse-ci223@users.noreply.github.com>
2023-11-25 21:09:21 +0000

    Added docstrings

--- parally/client.py
+++ parally/client.py
@@ -9,6 +9,16 @@ class Client:
     A simple client class that connects to a given host and port.
     """
     def __init__(self, host, port):
+        """
+        __init__ Initializes the client.
+
+        Parameters
+        ----------
+        host : str
+            The host to connect to.
+        port : int
+            The port to connect to.
+        """
         self._host = host
         self._port = port
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,41 +26,46 @@ class Client:
         self._input_parameters = {}
         self.function = None
 
-    def input_function(self, function):
+    def input_function(self, function) -> None:
+        """
+        input_function Sets the function to run on the server.
+
+        Parameters
+        ----------
+        function : function
+            The function to run on the server.
+        """
         self.function = function
 
-    def start(self):
-        # self._socket.connect((self._host, self._port))
-        # print(f"Connected to server {self._host}:{self._port}")
-        # while True:
-        #     print("Waiting for data...")
-        #     data = self._socket.recv(1024)
-        #     if not data:
-        #         continue
-        #     data = json.loads(data.decode())
-        #     print(f"Received data: {data}")
-        #     self._socket.sendall(json.dumps({'action': 'received'}).encode())
-        #     if data['action'] == 'run':
-        #         self._input_parameters = data['data']
-        #         print(f"Running function with parameters {self._input_parameters}")
-        #         try:
-        #             result = self.function(self._input_parameters)
-        #             to_send = json.dumps({'action': 'result', 'data': result})
-        #             self._socket.sendall(to_send.encode())
-        #         except Exception as e:
-        #             to_send = json.dumps({'action': 'error', 'error': str(e)})
-        #             self._socket.sendall(to_send.encode())
-        #     elif data['action'] == 'done':
-        #         continue
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self._host, self._port))
-            print(f"Connected to {self._host}:{self._port}")
-            while True:
-                data = s.recv(1024)
-                if not data:
-                    break
-                data = json.loads(data.decode())
-                print(data)
+    def start(self) -> None:
+        """
+        start Starts the client.
+        """
+        self._socket.connect((self._host, self._port))
+        print(f"Connected to server {self._host}:{self._port}")
+        while True:
+            print("Waiting for data...")
+            data = self._socket.recv(1024)
+            if not data:
+                continue
+            data = json.loads(data.decode())
+            print(f"Received data: {data}")
+            self._socket.sendall(json.dumps({'action': 'received'}).encode())
+            if data['action'] == 'run':
+                self._input_parameters = data['data']
+                print(f"Running function with parameters {self._input_parameters}")
+                try:
+                    result = self.function(self._input_parameters)
+                    to_send = json.dumps({'action': 'result', 'data': result})
+                    self._socket.sendall(to_send.encode())
+                except Exception as e:
+                    to_send = json.dumps({'action': 'error', 'error': str(e)})
+                    self._socket.sendall(to_send.encode())
+            elif data['action'] == 'done':
+                continue
 
-    def close(self):
+    def close(self) -> None:
+        """
+        close Closes the client.
+        """
         self._socket.close()
