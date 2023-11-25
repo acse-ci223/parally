f51c213aa24f6694b8a2d365c9c5b6abd33317c7
Chris Ioannidis <142539198+acse-ci223@users.noreply.github.com>
2023-11-25 14:38:19 +0000

    Added worker class

--- parally/server.py
+++ parally/server.py
@@ -1,6 +1,45 @@
 import socket
+import json
 
 __all__ = ['Server']
+
+class Worker:
+    def __init__(self, conn, addr):
+        self._conn = conn
+        self._addr = addr
+        self._is_running = False
+        self._result = None
+        self._assigned = False
+        self._received_data = bytes()
+
+    def terminate(self):
+        self._is_running = False
+        self._assigned = False
+        self._result = None
+        self._received_data = bytes()
+
+    def assign_task(self):
+        self._assigned = True
+
+    def run(self):
+        data_to_send = json.dumps({'action': 'run'})
+        self._conn.send(data_to_send.encode())
+        self._is_running = True
+        return True
+
+    def check_status(self):
+        data = self._conn.recv(1024)
+        if not data:
+            return
+        data += self._received_data
+        try:
+            data = json.loads(data.decode())
+            if data['action'] == 'result':
+                self._result = data['result']
+                return True
+        except json.decoder.JSONDecodeError:
+            self._received_data = data
+            return False
 
 
 class Server:
