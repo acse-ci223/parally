c6f50d7681264757cf19fd235b02c1a6993574b5
Chris Ioannidis <142539198+acse-ci223@users.noreply.github.com>
2023-11-25 14:46:32 +0000

    Added test and workers dict

--- parally/server.py
+++ parally/server.py
@@ -3,6 +3,7 @@ import json
 
 __all__ = ['Server']
 
+
 class Worker:
     def __init__(self, conn, addr):
         self._conn = conn
@@ -59,20 +60,21 @@ class Server:
         """
         self.host = host
         self.port = port
-        self.sock = None
+        self._sock = None
+        self._workers = {}
 
     def start(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind((self.host, self.port))
-        self.sock.listen(5)
-        self.sock.settimeout(1)
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock.bind((self.host, self.port))
+        self._sock.listen(5)
+        self._sock.settimeout(1)
 
     def accept(self):
         try:
-            client, address = self.sock.accept()
+            client, address = self._sock.accept()
             return client, address
         except socket.timeout:
             return None, None
 
     def close(self):
-        self.sock.close()
+        self._sock.close()
